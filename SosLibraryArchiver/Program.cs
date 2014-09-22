7acf006a9fe953c2b26051b124597bb482388900
Alex Povar <zvirja1@gmail.com>
2014-09-22 12:17:18 +0300

    Initial commit.

---
+++ SosLibraryArchiver/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace SosLibraryArchiver
+{
+  internal class Program
+  {
+    #region Public Methods and Operators
+
+    private static void PrintError(string message)
+    {
+      Console.WriteLine(message);
+      Console.ReadKey();
+    }
+
+    public static bool? IsX86Lib(string pFilePath)
+    {
+      ushort architecture = 0;
+      try
+      {
+        using (System.IO.FileStream fStream = new System.IO.FileStream(pFilePath, System.IO.FileMode.Open, System.IO.FileAccess.Read))
+        {
+          using (System.IO.BinaryReader bReader = new System.IO.BinaryReader(fStream))
+          {
+            if (bReader.ReadUInt16() == 23117) //check the MZ signature
+            {
+              fStream.Seek(0x3A, System.IO.SeekOrigin.Current); // seek to e_lfanew.
+              fStream.Seek(bReader.ReadUInt32(), System.IO.SeekOrigin.Begin); //Seek to the start of the NT header.
+              if (bReader.ReadUInt32() == 17744) // check the PE\0\0 signature.
+              {
+                fStream.Seek(20, System.IO.SeekOrigin.Current); // seek past the file header, and
+                architecture = bReader.ReadUInt16(); // read the magic number of the optional header.
+              }
+            }
+          }
+        }
+      }
+      catch (Exception)
+      {
+        return null;
+      }
+
+      if (architecture == 0x10b)
+      {
+        return true;
+      }
+
+      if (architecture == 0x20b)
+      {
+        return false;
+      }
+
+      return null;
+    }
+
+    #endregion
+
+    #region Methods
+
+    private static bool HandleRegistrationParam(string[] args)
+    {
+      bool isRegChangeCommand = false;
+      if (args[0].Equals("/regshell", StringComparison.OrdinalIgnoreCase))
+      {
+        RegistryHelper.SetRegistrationIsSwitchedOn(true);
+        isRegChangeCommand = true;
+      }
+      else if (args[0].Equals("/unregshell", StringComparison.OrdinalIgnoreCase))
+      {
+        RegistryHelper.SetRegistrationIsSwitchedOn(false);
+        isRegChangeCommand = true;
+      }
+
+      RegistryHelper.EnsureContextMenuRegistrationConsistent(Assembly.GetExecutingAssembly().Location);
+      return isRegChangeCommand;
+    }
+
+    private static void Main(string[] args)
+    {
+      if (args.Length != 1)
+      {
+        PrintError("More than one arg should be passed.");
+        return;
+      }
+
+      if (HandleRegistrationParam(args))
+      {
+        return;
+      }
+
+      string pathToDll = args[0];
+
+      ProcessLibrary(pathToDll, new HashSet<string>());
+    }
+
+    private static void ProcessLibrary(string pathToDll, HashSet<string> alreadyProcessed)
+    {
+      if (alreadyProcessed.Contains(pathToDll.ToLowerInvariant()))
+      {
+        PrintError("Stack overflow prevented. Unable to relocate assembly.");
+        return;
+      }
+
+      alreadyProcessed.Add(pathToDll.ToLowerInvariant());
+
+      string originalName = Path.GetFileNameWithoutExtension(pathToDll);
+      if (originalName == null)
+      {
+        PrintError("Cannot get name without extension.");
+        return;
+      }
+
+      string parentDir = Path.GetDirectoryName(pathToDll);
+      if (parentDir == null)
+      {
+        PrintError("Cannot resolve parent dir.");
+        return;
+      }
+
+      string newName;
+
+      int indexOfDot = originalName.IndexOf('.');
+      if (indexOfDot < 0)
+      {
+        bool? isX86 = IsX86Lib(pathToDll);
+        FileVersionInfo assemblyVersion = FileVersionInfo.GetVersionInfo(pathToDll);
+        var assemblyVersionString = string.Join(".",
+          new string[]
+          {
+            assemblyVersion.FileMajorPart.ToString(),
+            assemblyVersion.FileMinorPart.ToString(),
+            assemblyVersion.FileBuildPart.ToString(),
+            assemblyVersion.FilePrivatePart.ToString()
+          });
+
+        string bitnessString = isX86 != null ? (isX86.Value ? "X86" : "X64") : "UNK";
+
+
+        newName = originalName + "." + bitnessString + "." + assemblyVersionString;
+      }
+      else
+      {
+        newName = originalName.Substring(0, indexOfDot);
+      }
+
+      string newLoc = Path.Combine(parentDir, newName + Path.GetExtension(pathToDll));
+
+      if (File.Exists(newLoc))
+      {
+        ProcessLibrary(newLoc, alreadyProcessed);
+      }
+
+      if (File.Exists(newLoc))
+      {
+        PrintError("Cannot find place for library.");
+        return;
+      }
+
+      File.Move(pathToDll, newLoc);
+    }
+
+    #endregion
+  }
+}
---
+++ SosLibraryArchiver/RegistryHelper.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;
+
+namespace SosLibraryArchiver
+{
+  public static class RegistryHelper
+  {
+    #region Constants
+
+    private const string ContextMenuRegistrationValueName = "ContextMenuRegistrationEnabled";
+
+    private const string ShellApplicationPath = @"dllfile\shell\zArchive SOS Library";
+    private const string ShellCommandPath = ShellApplicationPath + "\\command";
+    private const string ToolSubKeyPath = "Software\\Zvirja\\LibraryArchiver";
+
+    #endregion
+
+    #region Public Methods and Operators
+
+    public static void EnsureContextMenuRegistrationConsistent(string pathToExe)
+    {
+      if (!GetRegistrationIsSwitchedOn())
+      {
+        RemoveContextMenuRegistration();
+        return;
+      }
+
+      bool validValueIsPresent = false;
+      string shellCommandValue = GetShellCommandValue(pathToExe);
+      RegistryKey commandKey = Registry.ClassesRoot.OpenSubKey(ShellCommandPath);
+      if (commandKey != null)
+      {
+        var currentCommandValue = commandKey.GetValue(string.Empty, string.Empty) as string;
+        if (!string.IsNullOrEmpty(currentCommandValue))
+        {
+          validValueIsPresent = currentCommandValue.Equals(shellCommandValue, StringComparison.OrdinalIgnoreCase);
+        }
+      }
+
+      if (!validValueIsPresent)
+      {
+        CreateContextMenuRegistration(shellCommandValue);
+      }
+    }
+
+
+    public static bool GetRegistrationIsSwitchedOn()
+    {
+      RegistryKey appKey = GetAppKey();
+      if (!appKey.GetValueNames().Contains(ContextMenuRegistrationValueName))
+      {
+        return false;
+      }
+
+      if (appKey.GetValueKind(ContextMenuRegistrationValueName) != RegistryValueKind.DWord)
+      {
+        return false;
+      }
+
+      return ((int)appKey.GetValue(ContextMenuRegistrationValueName, 0)) == 1;
+    }
+
+    public static void SetRegistrationIsSwitchedOn(bool value)
+    {
+      RegistryKey appKey = GetAppKey();
+      appKey.SetValue(ContextMenuRegistrationValueName, value ? 1 : 0, RegistryValueKind.DWord);
+    }
+
+    #endregion
+
+    #region Methods
+
+    private static void CreateContextMenuRegistration(string commandValue)
+    {
+      RegistryKey commandKey = Registry.ClassesRoot.CreateSubKey(ShellCommandPath);
+      commandKey.SetValue(string.Empty, commandValue, RegistryValueKind.String);
+    }
+
+    private static RegistryKey GetAppKey()
+    {
+      return Registry.CurrentUser.CreateSubKey(ToolSubKeyPath);
+    }
+
+    private static string GetShellCommandValue(string pathToExe)
+    {
+      string trimmedPath = pathToExe.Trim();
+      bool quoteDecorate = !trimmedPath.StartsWith("\"");
+
+      string commandPathFormatStr = quoteDecorate ? "\"{0}\" \"%1\"" : "{0} \"%1\"";
+      return string.Format(commandPathFormatStr, trimmedPath);
+    }
+
+    private static void RemoveContextMenuRegistration()
+    {
+      if (Registry.ClassesRoot.OpenSubKey(ShellApplicationPath) != null)
+      {
+        Registry.ClassesRoot.DeleteSubKeyTree(ShellApplicationPath);
+      }
+    }
+
+    #endregion
+  }
+}
